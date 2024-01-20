d0fb2899b37d47292fc03ec31f31ba64864c54c3
Emmalcd <joselemmanuel9@gmail.com>
2024-01-20 16:41:45 -0600

    Ejercicio con strings

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MiNombre = "Emmanuel";
+            string mensaje = $"Hola yo soy {MiNombre}";
+            string nuevoMensaje= mensaje.ToUpper() ;
+            Console.WriteLine(nuevoMensaje);
+            Console.ReadLine();
+        }
+    }
+}
